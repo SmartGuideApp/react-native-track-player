ff99f515dedc2358509d8442b4786b449a1a66a9
Filip Kinský <filip@filovo.net>
2022-12-22 15:18:35 +0100

    add Android 12 support via https://github.com/doublesymmetry/react-native-track-player/pull/1362#issuecomment-1043218428

--- android/src/main/java/com/guichaguri/trackplayer/service/MusicManager.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/MusicManager.java
@@ -192,7 +192,7 @@ public class MusicManager implements OnAudioFocusChangeListener {
                 new DefaultTrackSelector(), control);
 
         player.setAudioAttributes(new com.google.android.exoplayer2.audio.AudioAttributes.Builder()
-                .setContentType(C.CONTENT_TYPE_MUSIC).setUsage(C.USAGE_MEDIA).build());
+                .setContentType(C.CONTENT_TYPE_MUSIC).setUsage(C.USAGE_MEDIA).build(), false);
 
         return new LocalPlayback(service, this, player, cacheMaxSize);
     }
